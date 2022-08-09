40d65c317b7a0d5fd5f64ddd72feb8b94cd846dc
Aayush Dhungana <90859745+aayush-dhg@users.noreply.github.com>
2022-08-09 13:08:54 -0500

    added several components: cart, checkout, footer, header, home, productsingle, shop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
+const routes: Routes = [
+  { path:"", component:HomeComponent },
+  { path:"product-single", component:ProductsingleComponent },
+  { path:"cart", component:CartComponent },
+  { path:"checkout", component:CheckoutComponent },
+  { path:"shop", component:ShopComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
