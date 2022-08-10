aab100eb87b47d35957391e142f8a19362338519
Aayush Dhungana <90859745+aayush-dhg@users.noreply.github.com>
2022-08-10 16:10:58 -0500

    added email in sign up page, css needsto be done

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  customerForm: FormGroup;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]),
+      username: new FormControl(''),
+      password: new FormControl(''),
+      balance: new FormControl('', [Validators.pattern(/^[0-9 ]+$/)]),
+      securityQuestion: new FormControl(''),
+      securityAnswer: new FormControl('')
+    });
+  }
+
+  onFormSubmit(){
+
   }
 
 }
